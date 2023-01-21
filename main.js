3eae81412a9cfaba88435c9bbb3bab3189255a42
felipesilvaapd <felipe.silvaapd@gmail.com>
2023-01-20 23:18:23 -0300

    projeto concluido

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+   const elemento = document.querySelector(seletorAudio);
+
+   //!=(diferente), &&(e ou and)
+   if (elemento != null && elemento.localName === 'audio'){
+        elemento.play();
+     }
+    else{
+         alert('Elemento não encontrado ou seletor inválido')    
+    }
+
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -15,18 +24,35 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
         tocaSom(idAudio);
     }
 
-    tecla.onkeydown = function (e) {
-        tecla.classList.add('ativa');
-    }
+    tecla.onkeydown = function (evento) {
+        
+        console.log(evento)
 
+
+        if(evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+    
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 }
 
-const body = document.querySelector("body")
 
-body.addEventListener("keypress", (e) => {
-    if(e.key === "q") {
-        tocaSom("#som_tecla_pom")
-    } else if (e.key === "w") {
-        tocaSom("#som_tecla_clap")
-    }
-})
+
+
+
+
+
+
+// const body = document.querySelector("body")
+
+// body.addEventListener("keypress", (e) => {
+//     if(e.key === "q") {
+//         tocaSom("#som_tecla_pom")
+//     } else if (e.key === "w") {
+//         tocaSom("#som_tecla_clap")
+//     }
+// })
